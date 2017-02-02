deb02a0bc3b1a13f5ce4067a13b7e5ed837964c1
jeremyjochemskinexxtep <jeremy.jochemski@recursive-solution.com>
2017-02-02 23:05:18 +0100

    FRACTAL-01 : Setting base loading file controls

--- test/Container/ContainerTest.js
+++ test/Container/ContainerTest.js
@@ -6,14 +6,14 @@ chai.should();
 describe('Container', () => {
   it('construct() : must load only one configuration file in this case', (done) => {
     const container = new Container('test/Common/ConfigFiles/Config1.json');
-    Object.keys(container.objectFiles).length.should.equal(1);
-    Object.keys(container.objectType).length.should.equal(2);
+    Object.keys(container.loadedFiles).length.should.equal(1);
+    Object.keys(container.objectDefinition).length.should.equal(2);
     done();
   });
   it('construct() : must load two configuration file in this case', (done) => {
     const container = new Container('test/Common/ConfigFiles');
-    Object.keys(container.objectFiles).length.should.equal(1);
-    Object.keys(container.objectType).length.should.equal(4);
+    Object.keys(container.loadedFiles).length.should.equal(1);
+    Object.keys(container.objectDefinition).length.should.equal(4);
     done();
   });
 });
